0a09e7f0ea9399d98434a4e0dbf1d5cb47ff9f60
Paul1 <ana_mendes@sme.prefeitura.sp.gov.br>
2021-12-06 20:30:15 -0300

    Segundo commit

---
+++ CadastroEvento.js
@@ -0,0 +1,15 @@
+var Idade = 18
+if (Idade <18) {
+    console.log("Não permitimos eventos para menores de 18 anos")
+} else {
+    console.log("Insira a seguir o nome dos palestrantes")
+}
+
+let listaParticipantes = [ "Simone de Bouvoir", "Lélia Gonzalez", "Angela Davis"]
+
+if (listaParticipantes.length <100) {
+console.log("É possível cadastrar até 100 participantes no evento")
+} else {
+    console.log ("Você excedeu o número máximo de 100 participantes")
+}
+
